bbb04f067124695b0443e40178a8883afd6b5460
Bedirhan KURT <bedirhan.kurt@edu.finoku.com>
2025-05-05 15:20:16 +0300

    work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,70 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue';
-import BaseMain from './components/main/BaseMain.vue';
+import { shallowRef, type Component } from "vue";
+import Stopwatch from "./components/timers/Stopwatch.vue";
+import Countdown from "./components/timers/Countdown.vue";
 
+// Düzenlenecek !
+// import BaseTimer from "./components/timers/BaseTimer.vue";
+const currentComponent = shallowRef<null | any>(null);
 
-const currentComponent = shallowRef(BaseMain);
-function newComp(activeComponent: any) {
-  currentComponent.value = activeComponent
-}
-
+const setComponent = (activeComp: Component) => {
+  currentComponent.value = activeComp;
+};
 </script>
 
-
 <template>
-  <component :is="currentComponent" @countdown-comp="newComp" @back-main="newComp" @standart-countdown="newComp" />
+  <component :is="currentComponent" :setComponent="setComponent" />
+  <!--Not (1)** Sorun Çıkarırsa min-h-screen geri al  [1.div]-->
+  <div
+    v-if="currentComponent === null"
+    class="min-h-[40vw] bg-stars bg-cover bg-no-repeat overflow-hidden"
+  >
+    <!-- Header -->
+    <header class="overflow-hidden flex flex-col items-center justify-center">
+      <h1
+        class="text-center text-pColor uppercase tracking-[1vw] sm:tracking-[0.5vw] max-w-screen text-2xl pt-40 font-redhatFont"
+      >
+        Select Countdown Section
+      </h1>
+    </header>
+
+    <!-- Main -->
+    <main>
+      <div
+        class="flex justify-center text-center gap-4 sm:gap-16 mt-14 sm:mt-36 font-redhatFont"
+      >
+        <div class="">
+          <!-- Css ile geliştirilecek  [h2] ! -->
+          <h2 class="text-md">Stopwatch</h2>
+          <!--  svg veya başka iconlar ile gösterilecek [Button] ! // kronometre sayfasına yönlendirecek !  -->
+          <button
+            class="px-10 mt-5 rounded-md text-md text-pColor bg-countdownP transition-transform duration-500 hover:scale-110 tracking-widest hover:text-white"
+            @click="setComponent(Stopwatch)"
+          >
+            Select
+          </button>
+        </div>
+        <div class="">
+          <!-- Css ile geliştirilecek  [h2] ! -->
+          <h2 class="text-md">Countdown</h2>
+          <!--  svg veya başka iconlar ile gösterilecek [Button] ! // kronometre sayfasına yönlendirecek !  -->
+          <button
+            class="px-10 mt-5 rounded-md text-md text-pColor bg-countdownP transition-transform duration-500 hover:scale-110 tracking-widest hover:text-white"
+            @click="setComponent(Countdown)"
+          >
+            Select
+          </button>
+        </div>
+      </div>
+    </main>
+
+    <!-- Footer -->
+    <footer>
+      <div
+        class="socials bg-hills bg-cover bg-no-repeat overflow-hidden fixed bottom-0 w-full min-h-36 lg:min-h-60 md:min-h-40"
+      ></div>
+    </footer>
+  </div>
 </template>
 
 <style>
---
+++ src/components/timers/Countdown.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { onMounted, ref, onUnmounted } from "vue";
+import { defineProps } from "vue";
+const props = defineProps(["setComponent"]);
+
+const Days = ref<number | string | null>(null);
+const Hours = ref<number | string | null>(null);
+const Minutes = ref<number | string | null>(null);
+const Seconds = ref<number | string | null>(null);
+let intervalId: number;
+
+function calculationTime() {
+  const date = new Date("May 17,2025,00:00:00").getTime();
+  const nowDate = new Date().getTime();
+  const dateDifference = date - nowDate;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  let timeDays = Math.floor(dateDifference / days);
+  let timeHours: string | number = Math.floor((dateDifference % days) / hours);
+  let timeMinutes: string | number = Math.floor(
+    (dateDifference % hours) / minutes
+  );
+  let timeSeconds: string | number = Math.floor(
+    (dateDifference % minutes) / seconds
+  );
+
+  if (timeHours < 10) {
+    timeHours = "0" + timeHours;
+  }
+  if (timeMinutes < 10) {
+    timeMinutes = "0" + timeMinutes;
+  }
+  if (timeSeconds < 10) {
+    timeSeconds = "0" + timeSeconds;
+  }
+  Days.value = timeDays;
+  Hours.value = timeHours;
+  Minutes.value = timeMinutes;
+  Seconds.value = timeSeconds;
+}
+
+onMounted(() => {
+  calculationTime();
+  intervalId = setInterval(calculationTime, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+</script>
+
+<template>
+  <div class="min-h-screen bg-stars bg-cover bg-no-repeat overflow-hidden">
+    <header class="overflow-hidden flex flex-col items-center justify-center">
+      <button
+        class="text-sm font-medium text-white bg-countdownP hover:bg-gray-700 px-10 py-1 hover:px-16 transition-[padding] rounded-xl duration-500 rounded-b-full hover:text-pColor"
+        @click="props.setComponent(null)"
+      >
+        Back
+      </button>
+      <h1
+        class="text-center text-white uppercase tracking-[1vw] sm:tracking-[0.5vw] max-w-screen text-2xl pt-40 font-redhatFont"
+      >
+        Countdown
+      </h1>
+    </header>
+    <main>
+      <div
+        class="countdown flex justify-center text-center gap-2 sm:gap-16 mt-14 sm:mt-36 overflow-hidden"
+      >
+        <div class="time-box">
+          <p
+            class="w-20 sm:w-28 h-20 sm:h-28 flex items-center justify-center text-3xl sm:text-6xl rounded-full font-redhatFont bg-countdownP text-pColor"
+          >
+            {{ Days }}
+          </p>
+          <h3
+            class="uppercase tracking-widest text-xs font-redhatFont pt-2 text-cdh3Color"
+          >
+            Days
+          </h3>
+        </div>
+        <div class="time-box">
+          <p
+            class="w-20 sm:w-28 h-20 sm:h-28 flex items-center justify-center text-3xl sm:text-6xl rounded-full font-redhatFont bg-countdownP text-pColor"
+          >
+            {{ Hours }}
+          </p>
+          <h3
+            class="uppercase tracking-widest text-xs font-redhatFont pt-2 text-cdh3Color"
+          >
+            Hours
+          </h3>
+        </div>
+        <div class="time-box">
+          <p
+            class="w-20 sm:w-28 h-20 sm:h-28 flex items-center justify-center text-3xl sm:text-6xl rounded-full font-redhatFont bg-countdownP text-pColor"
+          >
+            {{ Minutes }}
+          </p>
+          <h3
+            class="uppercase tracking-widest text-xs font-redhatFont pt-2 text-cdh3Color"
+          >
+            Minutes
+          </h3>
+        </div>
+        <div class="time-box">
+          <p
+            class="w-20 sm:w-28 h-20 sm:h-28 flex items-center justify-center text-3xl sm:text-6xl rounded-full font-redhatFont bg-countdownP text-pColor"
+          >
+            {{ Seconds }}
+          </p>
+          <h3
+            class="uppercase tracking-widest text-xs font-redhatFont pt-2 text-cdh3Color"
+          >
+            Seconds
+          </h3>
+        </div>
+      </div>
+    </main>
+  </div>
+  <footer
+    class="socials bg-hills bg-cover bg-no-repeat overflow-hidden fixed bottom-0 w-full min-h-36 lg:min-h-60 md:min-h-40"
+  ></footer>
+</template>
+
+<style scoped></style>
